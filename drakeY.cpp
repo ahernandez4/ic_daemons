cf1f6baccbd059193244b6d636a2048b1b5e74a4
Drake Young <dyoung@odin.cs.csubak.edu>
2019-09-29 01:29:47 -0700

    Drake commited changes to drakeY.cpp

--- drakeY.cpp
+++ drakeY.cpp
@@ -1,7 +1,17 @@
 //Drake Young
+//Group Name: segFault
+//Instructor: Gordon Griesel
+//3350
+//date:    fall 2019
+//
+//Location for My code added to segFault 
+//group project
+//
+//NOTES: WIP
+
 #include "fonts.h"
 
-
+// Funtion to draw my name for the credits.
 void drawDY_Credits(int x, int y)
 {
     Rect D;
