f97e830f5894d571b3cda1b85c1b13525ed16005
Alex <alex@vcpea24fm>
2019-11-28 18:03:42 -0800

    added enemy header file to be used across any file

---
+++ Enemy.h
@@ -0,0 +1,16 @@
+#ifndef ENEMY_H
+#define ENEMY_H
+//maybe some of these should be private
+class Enemy{
+    private:
+    GLuint enemyTexture;
+    public:
+    Enemy(GLuint atexture);
+    int health;
+    int xpos;
+    int ypos;
+    void Draw();
+    void Spawn(int x, int y);
+};
+
+#endif
