bb3beb23725ae93626fe2ef93193915ab0b3c5b7
Alex <alex@vcpea24fm>
2019-11-30 17:27:17 -0800

    moved variables to public

--- GameScene.h
+++ GameScene.h
@@ -6,6 +6,9 @@ class GameScene{
 	int playerx;
 	int playery;
     public:
+    int prev_playerx;
+    int prev_playery;
+    bool deleteSoon;
     GameScene(){}
 	//GameScene(int*,int*);
 	virtual ~GameScene(){};
